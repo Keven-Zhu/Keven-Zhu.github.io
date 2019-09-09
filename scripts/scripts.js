06b217eab014a14b64037bbf391c20fd363a995c
Keven-Zhu <zhukeven86@gmail.com>
2019-09-09 18:29:25 -0400

    Added experience and projects page, various improvements and UX tweaks

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,31 +14,84 @@ window.onload = function(){
 };
 
 $(document).ready(function() {
-  var loaded=false;
+  var loaded1=false;
+  var loaded2=false;
+  var loaded3=false;
   $(window).scroll(function() {
-    if (!loaded && $(window).scrollTop() >  $(document).height() - $(window).height() - (screen.height / 2)) {
+    if (!loaded1 && $(window).scrollTop() >  $(document).height() - $(window).height() - (screen.height * 3)) {
       window.setTimeout(function(){
         document.getElementById('aboutme-header').className += " load";
-      }, 500);
+      }, 200);
   
       window.setTimeout(function(){
         document.getElementById('aboutme-body').className += " load";
-      }, 1000);
+      }, 700);
 
-      loaded=true;
+      loaded1=true;
     }
+    else if (!loaded2 && $(window).scrollTop() >  $(document).height() - $(window).height() - (screen.height * 2)) {
+      window.setTimeout(function(){
+        document.getElementById('experience-header').className += " load";
+      }, 200);
+  
+      window.setTimeout(function(){
+        document.getElementById('experience-container').className += " load";
+      }, 700);
+      loaded2=true;
+    }
+    else if (!loaded3 && $(window).scrollTop() >  $(document).height() - $(window).height() - (screen.height * 1)) {
+      window.setTimeout(function(){
+        document.getElementById('projects-header').className += " load";
+      }, 200);
+  
+      window.setTimeout(function(){
+        document.getElementById('projects-body').className += " load";
+      }, 700);
+      loaded3=true;
+    }
+});
 });
 
 $("a").on('click', function(event) {
-  if (this.hash !== "") {
-    event.preventDefault();
-    var hash = this.hash;
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function(){   
-      window.location.hash = hash;
-    });
+  if (event.target.id === "nav-link"){
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){   
+        window.location.hash = hash;
+      });
+    }
   }
 });
-});
 
+$(document).ready(function() {
+  $('#experience-material-tabs').each(function() {
+
+      var $active, $content, $links = $(this).find('a');
+
+      $active = $($links[0]);
+      $active.addClass('active');
+
+      $content = $($active[0].hash);
+
+      $links.not($active).each(function() {
+          $(this.hash).hide();
+      });
+
+      $(this).on('click', 'a', function(e) {
+
+          $active.removeClass('active');
+          $content.hide();
+
+          $active = $(this);
+          $content = $(this.hash);
+
+          $active.addClass('active');
+          $content.show();
+
+          e.preventDefault();
+      });
+  });
+});
